81559e28709d85304870c14166dfc0aebaa1b1b7
texascake <tiktodz@soulvibe-team.cloud>
2026-05-12 15:03:46 +0700

    Change rclone remote from Gdrive to queen [skip ci]

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 if [ -f ~/rom/out/target/product/X00TD/RROS-*.zip ]; then
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/X00TD/ && ls RROS-*.zip)"
-      rclone copy ~/rom/out/target/product/X00TD/RROS-*.zip Gdrive:rr -P
-      rclone copy ~/rom/out/target/product/X00TD/boot.img Gdrive:rr -P
+      rclone copy ~/rom/out/target/product/X00TD/RROS-*.zip queen:rr -P
+      rclone copy ~/rom/out/target/product/X00TD/boot.img queen:rr -P
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/X00TD/ && ls RROS-*.zip) Uploaded Successfully!"
 fi
